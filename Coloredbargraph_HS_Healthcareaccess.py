9bb57c9192a0be1c1ec9ec906ed54ad6ee823c02
Achyut Pillai <apillai@ucsd.edu>
2024-03-15 12:39:53 -0700

    Life Expectancy and Healthcare Files Updated

--- Coloredbargraph_HS_Healthcareaccess.py
+++ Coloredbargraph_HS_Healthcareaccess.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
-#pip install pivottablejs
 import pandas as pd
 import matplotlib.pyplot as plt
 from pivottablejs import pivot_ui
 
 plt.figure(figsize=(25, 12))
 # Load the dataset into a DataFrame
-data =pd.read_csv(r"C:\Users\riyaj\Downloads\__hap.csv",skipfooter=1, engine='python') 
+data =pd.read_csv("data/healthcare/_hap.csv",skipfooter=1, engine='python') 
 print("Shape before removing null values:", data.shape)
 
 # Remove rows with any null values
@@ -22,9 +15,6 @@ print("Shape after removing null values:", data.shape)
 
 # Create a pivot table using the pivot_ui function
 pivot_ui(data)
-
-
-# In[ ]:
 
 
 
--- Heatmap_code_betweenLE&HS_final.py
+++ Heatmap_code_betweenLE&HS_final.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd   #importing necessary libraries
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv(r"C:\Users\riyaj\Downloads\__life.csv")
+df = pd.read_csv("data/lifeExpectancy/_life.csv")
 #print(df)
 
 # Select only the numeric columns
@@ -23,7 +17,7 @@ rounded_numeric_columns = numeric_columns.round().astype(int)
 rounded_df = pd.concat([df.drop(columns=numeric_columns.columns), rounded_numeric_columns], axis=1)
 
 # Pivot the DataFrame for the heatmap
-heatmap_data  = rounded_df.pivot_table(index='Continent', columns='Happiness score', values='Healthy life expectancy')
+heatmap_data  = rounded_df.pivot_table(index='continents', columns='Happiness score', values='Healthy life expectancy')
 heatmap_data_filled = heatmap_data.bfill(axis=1)
 heatmap_data_filled = heatmap_data_filled.reindex(columns=heatmap_data.columns[::-1])
 
@@ -34,16 +28,3 @@ plt.subplot(1, 2, 1)
 sns.heatmap(heatmap_data_filled.T,  annot=False,  fmt='.1f',cmap="crest", cbar_kws={'label': 'Healthy life expectancy'},linewidth=.5)
 plt.tight_layout()
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
