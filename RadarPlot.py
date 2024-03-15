2382cfcc0b55601cfe8ddf2ad49439fe4ed1a06a
Tripti Chanda <triptic.ch16@gmail.com>
2024-03-15 12:05:27 -0700

    Radar and Sunburst entry

---
+++ RadarPlot.py
@@ -0,0 +1,88 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
+import seaborn as sns
+import plotly.graph_objects as go
+
+
+df = pd.read_csv('WHR2023.csv')
+df1=pd.read_csv('world-happiness-report.csv')
+import warnings
+warnings.filterwarnings("ignore")
+
+df.head()
+df1.head()
+color=["#f94144","#f3722c","#f8961e","#f9c74f","#90be6d","#43aa8b","#577590"]
+sns.palplot(color)
+fig = plt.figure(figsize=(15, 8))
+g = gs.GridSpec(ncols=1, nrows=2, figure=fig)
+plt.suptitle("Top 10 and Bottom 10 Countries in Happiness Index 2023", family='Serif', weight='bold', size=20)
+
+# Plot for Top 10 Countries
+ax1 = plt.subplot(g[0, 0])
+top_10 = df.head(10)
+sns.barplot(data=top_10, x='Ladder score', y='Country name', color=color[4], ax=ax1)
+ax1.xaxis.set_visible(False)
+ax1.annotate("Top 10 countries in Happiness index", xy=(8, 2), family='Serif', weight='bold', size=12)
+for index, value in enumerate(top_10['Ladder score']):
+    ax1.text(value, index, str(value), color='black', ha="left", va="center")
+
+# Plot for Bottom 10 Countries
+ax2 = plt.subplot(g[1, 0], sharex=ax1)
+bot_10 = df.tail(10)
+sns.barplot(data=bot_10, x='Ladder score', y='Country name', color=color[0], ax=ax2)
+ax2.annotate("Bottom 10 countries in Happiness index", xy=(8, 2), family='Serif', weight='bold', size=12)
+for index, value in enumerate(bot_10['Ladder score']):
+    ax2.text(value, index, str(value), color='black', ha="left", va="center")
+
+for s in ['left', 'right', 'top', 'bottom']:
+    ax1.spines[s].set_visible(False)
+    ax2.spines[s].set_visible(False)
+
+# Save the figure
+plt.savefig("top_and_bottom_countries_happiness_2023.png", dpi=300, bbox_inches='tight')
+
+plt.show()
+
+df.rename(columns={'Generosity': 'Air Quality Index'}, inplace=True)
+df1.rename(columns={'Generosity': 'Air Quality Index'}, inplace=True)
+
+
+
+categories = ['Ladder score','Logged GDP per capita','Social support','Healthy life expectancy', 'Freedom to make life choices', 'Air Quality Index', 'Perceptions of corruption']
+
+r1=[df[each][df["Country name"]=="Afghanistan"].mean()/df[each].max()  for each in categories]
+r2=[df[each][df["Country name"]=="Finland"].mean()/df[each].max()  for each in categories]
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatterpolar(
+      r=r1,
+      theta=categories,
+      fill='toself',
+      name='Afghanistan'
+))
+fig.add_trace(go.Scatterpolar(
+      r=r2,
+      theta=categories,
+      fill='toself',
+      name='Finland'
+))
+
+fig.update_layout(
+  polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 1]
+    )),
+  showlegend=True
+)
+
+fig.write_image("radar_comparison_plot.png")
+fig.show()
+
+
+
+
+
---
+++ Sunburst.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#pip install -U kaleido
+
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, normalize
+from sklearn.cluster import KMeans
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.offline import iplot
+import warnings
+
+
+# In[2]:
+
+
+happy_2023 = pd.read_csv("WHR2023.csv")
+country_mapping = pd.read_csv("continents.csv")
+
+# Dropping irrelevant columns
+country_mapping.drop('alpha-2', inplace=True, axis=1)
+
+# Remove all columns between column name 'country-code' to 'iso_3166-2' 
+country_mapping = country_mapping.drop(country_mapping.loc[:, 'country-code':'iso_3166-2'].columns, axis=1)
+
+# Remove all columns between column name 'intermediate-region' to 'intermediate-region-code' 
+country_mapping = country_mapping.drop(country_mapping.loc[:, 'intermediate-region':'intermediate-region-code'].columns, axis=1)
+country_mapping.head()
+
+#Rename the columns for consistency
+country_mapping = country_mapping.rename({'name':'country','alpha-3':'iso_alpha','sub-region':'sub_region'}, axis =1)
+
+
+# In[3]:
+
+
+happy_2023.loc[happy_2023.duplicated()]
+
+# Remove all columns between column name 'Ladder score in Dystopia' to 'Dystopia + residual' 
+happy_2023 = happy_2023.drop(happy_2023.loc[:, 'Ladder score in Dystopia':'Dystopia + residual'].columns, axis=1)
+
+# Remove all columns between column name 'Standard error of ladder score' to 'lowerwhisker' 
+happy_2023 = happy_2023.drop(happy_2023.loc[:, 'Standard error of ladder score':'lowerwhisker'].columns, axis=1)
+
+happy_2023['rank'] = happy_2023['Ladder score'].rank(ascending=False)
+happy_2023['rank'] = happy_2023['rank'].astype(int)
+
+
+# In[4]:
+
+
+happy_df_2023 = happy_2023.rename({'Country name':'country','Standard error of ladder score':'standard_error_of_ladder_score'
+                                   , 'Ladder score':'happiness_score','Happiness score':'happiness_score'
+                                   , 'Logged GDP per capita':'gdp_per_capita','Social support':'social_support'
+                                   , 'Healthy life expectancy':'healthy_life_expectancy'
+                                   , 'Freedom to make life choices':'freedom_to_make_life_choices'
+                                   , 'Generosity':'generosity','Perceptions of corruption':'perceptions_of_corruption'
+                                   , 'Explained by: Freedom to make life choices':'freedom_to_make_life_choices'
+                                   , 'Explained by: Generosity':'generosity'
+                                   , 'Explained by: Perceptions of corruption':'perceptions_of_corruption'}, axis =1)
+happy_df_2023.head()
+
+
+# In[5]:
+
+
+#Lets change the names in the country_mapping df to match those of the happiness report where appropriate
+
+#Turkey ----> Turkiye
+country_mapping['country'] = country_mapping['country'].str.replace('Turkey', 'Turkiye', regex=True)
+#Palestine, State of ----> State of Palestine
+country_mapping['country'] = country_mapping['country'].str.replace('Palestine, State of', 'State of Palestine', regex=True)
+#Côte D'Ivoire ----> Ivory Coast
+country_mapping['country'] = country_mapping['country'].str.replace("Côte D'Ivoire", 'Ivory Coast', regex=True)
+#Macedonia ----> North Macedonia
+country_mapping['country'] = country_mapping['country'].str.replace('Macedonia', 'North Macedonia', regex=True)
+#Hong Kong ----> Hong Kong S.A.R. of China
+country_mapping['country'] = country_mapping['country'].str.replace('Hong Kong', 'Hong Kong S.A.R. of China', regex=True)
+#Taiwan ----> Taiwan Province of China
+country_mapping['country'] = country_mapping['country'].str.replace('Taiwan', 'Taiwan Province of China', regex=True)
+#Czech Republic ----> Czechia
+country_mapping['country'] = country_mapping['country'].str.replace('Czech Republic', 'Czechia', regex=True)
+
+
+# In[6]:
+
+
+#Merge the dataframes again
+happy_region_df = happy_df_2023.merge(country_mapping, on='country', how='left')
+
+
+# In[7]:
+
+
+#Lets fix the nulls in the last four rows manually
+nan_region_rows = happy_region_df[happy_region_df['region'].isnull()]
+nan_region_rows
+
+
+# In[8]:
+
+
+#Manually updating region, sub_region and iso code for Kosovo
+happy_region_df.loc[33,'region'] = 'Europe'
+happy_region_df.loc[33,'sub_region'] = 'Southern Europe'
+happy_region_df.loc[33,'iso_alpha'] = 'XXK'
+
+#Manually updating region, sub_region and iso code for Bosnia and Herzegovina
+happy_region_df.loc[70,'region'] = 'Europe'
+happy_region_df.loc[70,'sub_region'] = 'Southern Europe'
+happy_region_df.loc[70,'iso_alpha'] = 'BIH'
+
+#Manually updating region, sub_region and iso code for Congo (Brazzaville)
+happy_region_df.loc[85,'region'] = 'Africa'
+happy_region_df.loc[85,'sub_region'] = 'Sub-Saharan Africa'
+happy_region_df.loc[85,'iso_alpha'] = 'COG'
+
+#Manually updating region, sub_region and iso code for Congo (Kinshasa)
+happy_region_df.loc[132,'region'] = 'Africa'
+happy_region_df.loc[132,'sub_region'] = 'Sub-Saharan Africa'
+happy_region_df.loc[132,'iso_alpha'] = 'COD'
+
+
+# In[11]:
+
+
+import plotly.express as px
+
+# Assuming happy_region_df and other necessary variables are already defined
+
+fig = px.sunburst(data_frame=happy_region_df,
+                  path=["region", "sub_region", "country"],
+                  values="happiness_score",
+                  color="happiness_score",
+                  color_continuous_scale='Viridis',
+                  width=1000, 
+                  height=1000,
+                  title='Happiness score sunburst - region / sub region / country')
+
+# Here's how you update the legend title
+fig.update_layout(coloraxis_colorbar_title='Happiness Score')
+
+fig.write_image(file='Sunburst.png', format='png')
+fig.show()
+
+
+# In[ ]:
+
+
+
+
